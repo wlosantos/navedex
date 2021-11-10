acadb63b5c8883f1a8913628824e996c3c4a6d3c
Wendel Lopes <wendelopes@msn.com>
2021-11-10 13:49:11 -0300

    implemented test project model

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Project, :focus, type: :model do
+  
+  describe 'attributes must be present' do
+    it { is_expected.to respond_to :name }
+    it { is_expected.to respond_to :nave_id }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to :nave }
+  end
+
+  describe 'persist database' do
+    context 'successful' do
+      let(:nave) { create(:nave) }
+      let(:project) { build(:project, nave: nave) }
+
+      it { expect(project).to be_valid }
+    end
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :nave_id }
+  end
+end
